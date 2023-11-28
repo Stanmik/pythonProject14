2ab211d125dfb3c5f1152a3c3d65f8019ef3c3f4
Stanislav Mikhaylovskiy <stasmikhal@gmail.com>
2023-11-29 01:39:49 +0300

    New version

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+# Задача 44: В ячейке ниже представлен код,
+# генерирующий DataFrame, которая состоит всего из 1 столбца.
+# Ваша задача перевести его в one hot вид.
+# Сможете ли вы это сделать без get_dummies?
+#
+# import random
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# random.shuffle(lst)
+# data = pd.DataFrame({'whoAmI':lst})
+# data.head()
+
 import random
 import pandas as pd
 
-# Создание DataFrame
+# Создаем DataFrame
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
+data = pd.DataFrame({'whoAmI': lst})
 
-# Создание нового столбца для каждого уникального значения в 'whoAmI'
+# Создаем новый столбец для каждого уникального значения в whoAmI
 for value in data['whoAmI'].unique():
    data[value] = (data['whoAmI'] == value).astype(int)
 
-# Удаление столбца 'whoAmI'
+# Удаляем столбец whoAmI
 data = data.drop('whoAmI', axis=1)
 
 print(data.head())
